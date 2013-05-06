0eaff3ac932dfb1164ff3583162da05b5b94443a
Macarena Pérez <macarena.perez@usach.cl>
2013-05-06 17:40:00 -0300

    lala

--- ServidorBB/src/interfaz/InterfazServidor.java
+++ ServidorBB/src/interfaz/InterfazServidor.java
@@ -10,14 +10,11 @@ public interface InterfazServidor extends Remote{
     public Vector<String> EmpleadoPerfil(int id) throws RemoteException;
     public Vector<String> ClienteUser(int id) throws RemoteException;
     public Vector<String> EmpleadoUser(int id) throws RemoteException;
-<<<<<<< HEAD
     public Vector<String> ClienteArriendo(int id) throws RemoteException;
     public void cambioPass(int idUsuario, String pass) throws RemoteException;
-=======
     public void nuevoUser(String user, String pass, String rol) throws RemoteException;
     /*public Vector<String> ClienteArriendo(int id) throws RemoteException;
-    */public void cambioPass(int idUsuario, String pass) throws RemoteException;
->>>>>>> vistas de cosas
+    */
     public Vector<String> EmpleadoCliente (int idEmpleado) throws RemoteException;
     public void registrarCliente(InterfazCliente cliente, String Nombre) throws RemoteException;
     public void desregistrarCliente(InterfazCliente cliente, String Nombre) throws RemoteException;
